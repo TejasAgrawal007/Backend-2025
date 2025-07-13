0a5198e96ceaf4a3292719c4abff0c16b3e0a098
Tejas Agrawal <tejasagrawal36@gmail.com>
2025-07-13 13:35:54 +0530

    update Server.js

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/about', (req, res) => {
+  res.send('About')
+})
+
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
